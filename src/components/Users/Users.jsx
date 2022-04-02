4876a19988f0a5db6dad0c727760c8169bbc43ce
Illya Romaniv <mippikzilla@gmail.com>
2022-04-02 19:47:41 +0300

    users class component(later refactor this component to hooks)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,9 +3,10 @@ import s from './Users.module.css'
 import axios from "axios";
 import profilePic from "../../assets/photos/default-profile-pic.jpg"
 
-const Users = (props) => {
+class Users extends React.Component {
 
-    if (props.users.length === 0) {
+    constructor(props) {
+        super(props);
         axios
             .get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
@@ -13,33 +14,35 @@ const Users = (props) => {
             })
     }
 
-    return (
-        <div className={s.friends}>
-            {props.users.map(u => {
-                return (
-                    <div key={u.id} className={s.friends_element}>
-                        <div className={s.left_content}>
-                            <img src={u.photos.small != null ? u.photos.small : profilePic} alt="user_picture"/>
-                            <button
-                                onClick={() => (props.toggleFollow(u.id))}>{u.followed ? 'Unfollow' : 'Follow'}</button>
-                        </div>
-
-                        <div className={s.right_content}>
-                            <div className={s.user_info}>
-                                <a href={`/users/`+u.id}>{u.name}</a>
-                                <p>{u.status}</p>
+    render() {
+        return (
+            <div className={s.friends}>
+                {this.props.users.map(u => {
+                    return (
+                        <div key={u.id} className={s.friends_element}>
+                            <div className={s.left_content}>
+                                <img src={u.photos.small != null ? u.photos.small : profilePic} alt="user_picture"/>
+                                <button
+                                    onClick={() => (this.props.toggleFollow(u.id))}>{u.followed ? 'Unfollow' : 'Follow'}</button>
                             </div>
 
-                            <div className={s.user_country}>
-                                <p>none</p>
-                                <p>none</p>
+                            <div className={s.right_content}>
+                                <div className={s.user_info}>
+                                    <a href={`/users/`+u.id}>{u.name}</a>
+                                    <p>{u.status}</p>
+                                </div>
+
+                                <div className={s.user_country}>
+                                    <p>Ukraine</p>
+                                    <p>Kyiv</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )
-            })}
-        </div>
-    )
+                    )
+                })}
+            </div>
+        )
+    }
 }
 
 export default Users;
