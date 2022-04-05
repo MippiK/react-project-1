c3078439ade3db621744c1724e2e4bea421029e3
Illya Romaniv <mippikzilla@gmail.com>
2022-04-05 19:43:16 +0300

    class users componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import s from './Users.module.css'
 import axios from "axios";
 import profilePic from "../../assets/photos/default-profile-pic.jpg"
+import {NavLink} from "react-router-dom";
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-        axios
-            .get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-                props.setUsers(response.data.items);
-            })
+    componentDidMount() {
+        if (this.props.users.length === 0) {
+            axios
+                .get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    this.props.setUsers(response.data.items);
+                })
+        }
     }
 
     render() {
@@ -28,7 +30,7 @@ class Users extends React.Component {
 
                             <div className={s.right_content}>
                                 <div className={s.user_info}>
-                                    <a href={`/users/`+u.id}>{u.name}</a>
+                                    <NavLink to={`/users/`+u.name.toLowerCase()}>{u.name}</NavLink>
                                     <p>{u.status}</p>
                                 </div>
 
